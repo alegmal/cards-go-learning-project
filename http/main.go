fc33cdc96faa2fbb2bcba12e280f3b9bd3dca444
Aleg Malinovsky <6178232+alegmal@users.noreply.github.com>
2023-06-08 12:20:45 +0300

    write interface

--- http/main.go
+++ http/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"io"
 	"os"
 	"fmt"
 	"net/http"
 )
+
+type logWriter struct {}
 
 func main()  {
 	resp, err := http.Get("http://www.google.com")
@@ -13,5 +16,19 @@ func main()  {
 		fmt.Println("error: ", err)
 		os.Exit(1)
 	}
-	fmt.Println(resp)
+
+	// bs := make([]byte, 999999)
+	// resp.Body.Read(bs)
+	// fmt.Println(string(bs))
+
+	lw := logWriter{}
+	io.Copy(lw, resp.Body)
+	// io.Copy(os.Stdout, resp.Body)
+}
+
+func (logWriter) Write(bs []byte) (n int, err error){
+	fmt.Println(string(bs))
+	fmt.Printf("just wrote %v bytes",len(bs))
+	
+	return len(bs), nil
 }
